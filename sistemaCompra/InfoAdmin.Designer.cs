d23e0a016917429dbed2e03488e68006777b2d2e
schweb04 <manelequintero@gmail.com>
2023-11-04 10:49:32 -0400

    Cambios diseño

--- sistemaCompra/InfoAdmin.Designer.cs
+++ sistemaCompra/InfoAdmin.Designer.cs
@@ -34,6 +34,8 @@
             lblImportacion = new Label();
             lblExportacion = new Label();
             lblControlDescripcion = new Label();
+            lblImportacionDescripcion = new Label();
+            label1 = new Label();
             SuspendLayout();
             // 
             // lblBienvenido
@@ -59,7 +61,7 @@
             // lblControl
             // 
             lblControl.Font = new Font("OCR A Extended", 12F, FontStyle.Italic, GraphicsUnit.Point);
-            lblControl.Location = new Point(34, 332);
+            lblControl.Location = new Point(42, 161);
             lblControl.Name = "lblControl";
             lblControl.Size = new Size(317, 43);
             lblControl.TabIndex = 2;
@@ -68,7 +70,7 @@
             // lblImportacion
             // 
             lblImportacion.Font = new Font("OCR A Extended", 12F, FontStyle.Italic, GraphicsUnit.Point);
-            lblImportacion.Location = new Point(401, 332);
+            lblImportacion.Location = new Point(408, 161);
             lblImportacion.Name = "lblImportacion";
             lblImportacion.Size = new Size(258, 43);
             lblImportacion.TabIndex = 3;
@@ -77,7 +79,7 @@
             // lblExportacion
             // 
             lblExportacion.Font = new Font("OCR A Extended", 12F, FontStyle.Italic, GraphicsUnit.Point);
-            lblExportacion.Location = new Point(714, 332);
+            lblExportacion.Location = new Point(728, 161);
             lblExportacion.Name = "lblExportacion";
             lblExportacion.Size = new Size(258, 43);
             lblExportacion.TabIndex = 4;
@@ -85,17 +87,41 @@
             // 
             // lblControlDescripcion
             // 
-            lblControlDescripcion.Location = new Point(57, 406);
+            lblControlDescripcion.Font = new Font("OCR A Extended", 9F, FontStyle.Italic, GraphicsUnit.Point);
+            lblControlDescripcion.Location = new Point(42, 387);
             lblControlDescripcion.Name = "lblControlDescripcion";
-            lblControlDescripcion.Size = new Size(259, 61);
+            lblControlDescripcion.Size = new Size(317, 61);
             lblControlDescripcion.TabIndex = 5;
-            lblControlDescripcion.Text = "label5";
+            lblControlDescripcion.Text = "Puedes ver la lista de Usuarios, Productos y Clientes guardados en el sistema. También podrás agregar, editar y eliminar de la lista.";
+            lblControlDescripcion.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // lblImportacionDescripcion
+            // 
+            lblImportacionDescripcion.Font = new Font("OCR A Extended", 9F, FontStyle.Italic, GraphicsUnit.Point);
+            lblImportacionDescripcion.Location = new Point(380, 387);
+            lblImportacionDescripcion.Name = "lblImportacionDescripcion";
+            lblImportacionDescripcion.Size = new Size(286, 61);
+            lblImportacionDescripcion.TabIndex = 6;
+            lblImportacionDescripcion.Text = "Puedes importar desde tu computador archivos del tipo .csv que contengan datos de Usuario, Producto y Cliente, siguiendo el formato especificado.";
+            lblImportacionDescripcion.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("OCR A Extended", 9F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(700, 387);
+            label1.Name = "label1";
+            label1.Size = new Size(286, 61);
+            label1.TabIndex = 7;
+            label1.Text = "Puedes exportar los datos de Usuario, Producto y Cliente que se encuentren guardados en el programa, a través de una copia de ellos como archivos del tipo .csv.";
+            label1.TextAlign = ContentAlignment.TopCenter;
             // 
             // InfoAdmin
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1032, 476);
+            Controls.Add(label1);
+            Controls.Add(lblImportacionDescripcion);
             Controls.Add(lblControlDescripcion);
             Controls.Add(lblExportacion);
             Controls.Add(lblImportacion);
@@ -116,5 +142,7 @@
         private Label lblImportacion;
         private Label lblExportacion;
         private Label lblControlDescripcion;
+        private Label lblImportacionDescripcion;
+        private Label label1;
     }
 }
